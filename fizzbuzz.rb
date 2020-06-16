80c7937e134faef658fbf77f95114f30b2c273f0
Jina Strickland <jina30@gmail.com>
2020-06-16 22:07:50 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,6 +2,14 @@
 # See README.md for instructions on how to do this
 def fizzbuzz(int)
   if int % 3 == 0
-    "Fizz"
-  end
+    puts "Fizz"
+
+  elsif int % 5 == 0
+    puts "Buzz"
+
+  elsif int % 3 && int % 5
+    puts "FizzBuzz"
+
+  else
+    puts nil
 end
